18f97fa2bc069401b3d0547ab8517e3e11dcdfb3
wj-Mcat <1435130236@qq.com>
2022-06-24 01:39:37 +0000

    complete the basic url link payload

--- src/osschat/url_link.py
+++ src/osschat/url_link.py
@@ -1,30 +1,81 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Dict, List
 from github import Github
 from github.Repository import Repository
+
 from github.PullRequest import PullRequest
+from github.PullRequestComment import PullRequestComment
+
 from github.Issue import Issue
 from github.IssueComment import IssueComment
+
 from github.NamedUser import NamedUser
 
 from wechaty_puppet.schemas.url_link import UrlLinkPayload
 
 
-def get_user_thumbnail_url(user_id: str) -> str:
-    user = NamedUser     
-
-class GithubInfo:
+class GithubUrlLink:
     def __init__(self, token: str):
         self._github = Github(login_or_token=token)
+        self._repositories: Dict[str, Repository] = {}
+
+    def get_repo(self, repo_name: str) -> Repository:
+        if repo_name in self._repositories:
+            return self._repositories[repo_name]
+        
+        repo = self._github.get_repo(repo_name)
+        self._repositories[repo_name] = repo
+        return repo
     
-    def get_short_description_of_pr(self, repo_name, pr_id: int) -> UrlLinkPayload:
-        repo: Repository = self._github.get_repo(repo_name)
+    def get_pr_payload(self, repo_name, pr_id: int) -> UrlLinkPayload:
+        repo: Repository = self.get_repo(repo_name)
         pull_request: PullRequest = repo.get_pull(pr_id)
         payload = UrlLinkPayload(
-            url=pull_request.url,
+            url=pull_request.html_url,
             title=pull_request.title,
             description=pull_request.body,
             thumbnailUrl=pull_request.user.avatar_url
         )
         return payload
-     
+    
+    def get_pr_comment_payload(self, repo_name: str, pr_id: int, comment_id: int, comment_type: str = 'issue') -> UrlLinkPayload:
+        repo: Repository = self.get_repo(repo_name)
+        pull_request: PullRequest = repo.get_pull(pr_id)
+        if comment_type == 'issue':
+            comment: PullRequestComment = pull_request.get_issue_comment(comment_id)
+        else:
+            comment: PullRequestComment = pull_request.get_review_comment(comment_id)
+
+        payload = UrlLinkPayload(
+            url=comment.html_url,
+            title=pull_request.title,
+            description=comment.body,
+            thumbnailUrl=comment.user.avatar_url
+        )
+        return payload  
+
+    def get_issue_payload(self, repo_name: str, issue_id: int) -> UrlLinkPayload:
+        repo: Repository = self.get_repo(repo_name)
+        issue: Issue = repo.get_issue(issue_id)
+        payload = UrlLinkPayload(
+            url=issue.html_url,
+            title=issue.title,
+            description=issue.body,
+            thumbnailUrl=issue.user.avatar_url
+        )
+        return payload  
+    
+    def get_issue_comment_payload(self, repo_name, issue_id: int, comment_id: int) -> UrlLinkPayload:
+        repo: Repository = self.get_repo(repo_name)
+        issue: Issue = repo.get_issue(issue_id)
+        comment: IssueComment = issue.get_comment(comment_id)
+
+        payload = UrlLinkPayload(
+            url=comment.html_url,
+            title=issue.title,
+            description=comment.body,
+            thumbnailUrl=comment.user.avatar_url
+        )
+        return payload  
+    
--- tests/test_url_link.py
+++ tests/test_url_link.py
@@ -1,8 +1,36 @@
-from osschat.url_link import GithubInfo
+import pytest
+from github.Repository import Repository
+
+from osschat.url_link import GithubUrlLink 
 
 
-def test_payload():
-    github_info = GithubInfo(None)
-    payload = github_info.get_short_description_of_pr('wechaty/python-wechaty', pr_id=331)
+@pytest.fixture
+def github_url_link() -> GithubUrlLink:
+    return GithubUrlLink(None)
+
+
+def test_pr_payload(github_url_link: GithubUrlLink):
+    payload = github_url_link.get_pr_payload('wechaty/python-wechaty', pr_id=331)
+    assert payload.url == 'https://github.com/wechaty/python-wechaty/pull/331'
     assert payload.title == 'improve the qrcode printed at terminal'
-    
+
+def test_pr_comment_payload(github_url_link: GithubUrlLink):
+    pr_id, comment_id = 4, 594452486
+
+    payload = github_url_link.get_pr_comment_payload('wechaty/python-wechaty', pr_id=pr_id, comment_id=comment_id)
+    assert payload.title == 'add quick test'
+    assert payload.url == f'https://github.com/wechaty/python-wechaty/pull/{pr_id}#issuecomment-{comment_id}'
+    assert 'Great work' in payload.description
+
+def test_issue_payload(github_url_link: GithubUrlLink):
+    payload = github_url_link.get_issue_payload('wechaty/python-wechaty', issue_id=5)
+
+    assert payload.url == 'https://github.com/wechaty/python-wechaty/issues/5'
+    assert payload.title == 'disucss the structure of starting python-wechaty'
+    assert 'code structure' in payload.description
+
+
+def test_issue_comment_payload(github_url_link: GithubUrlLink):
+    payload = github_url_link.get_issue_comment_payload('wechaty/python-wechaty', issue_id=5, comment_id=596385964)
+
+    assert 'Recently, I wrote async/await code in python' in payload.description
